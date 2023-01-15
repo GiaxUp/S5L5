efa5d35602b1b32d9fe27b56e40f6ed8e8f12f93
Giacomo <giax.95@live.it>
2023-01-15 17:23:36 +0100

    Aggiornamento animazione.

--- project.js
+++ project.js
@@ -1,3 +1,20 @@
+let lettera = document.querySelectorAll("svg g[stroke-linecap='butt']");
+console.log(lettera);
+let intervallo = setInterval(() => {
+  let randomNumber = Math.floor(Math.random() * lettera.length);
+  let ranLett = lettera[randomNumber];
+  ranLett.getElementsByClassName.transition = "1.2s";
+  if (ranLett.getAttribute("opacity") == 1) {
+    ranLett.setAttribute("opacity", "0");
+  } else {
+    ranLett.setAttribute("opacity", "1");
+  }
+}, 40);
+
+const fermati = () => {
+  clearInterval(intervallo);
+};
+
 let element = document.getElementById("navHeader").offsetHeight;
 console.log(element);
 const navbar = document.getElementById("navBar");
